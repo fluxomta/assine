4475f6cee326936bc021a848b3ada90cddc264b4
Tales Hs <webtailss@gmail.com>
2024-09-22 20:06:37 -0300

    remove curso from pricing

--- wp-content/themes/storefront-child/inc/woo/index.php
+++ wp-content/themes/storefront-child/inc/woo/index.php
@@ -1,4 +1,4 @@
 <?php
 
 // Inclui a classe WooCommerce_Pricing que gerencia as funções de preço e mensagens
-require_once get_stylesheet_directory() . '/inc/api/class-custom-api-controller.php';
+require_once get_stylesheet_directory() . '/inc/woo/class-woocommerce-pricing.php';
