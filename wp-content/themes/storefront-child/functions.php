e2e7e15f643cbfdc7e14faaef18b2c5ad9726f51
Tales Hs <webtailss@gmail.com>
2024-09-22 20:22:40 -0300

    update cors

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,7 +1,17 @@
 <?php
+
+add_action('after_setup_theme', function() {
+    if (getenv('JWT_AUTH_SECRET_KEY') && !defined('JWT_AUTH_SECRET_KEY')) {
+        define('JWT_AUTH_SECRET_KEY', getenv('JWT_AUTH_SECRET_KEY'));
+    }
+});
+
+
 // Inclui arquivos de funções
 require_once get_stylesheet_directory() . '/inc/cors.php';
 require_once get_stylesheet_directory() . '/inc/enqueue-scripts.php';
 require_once get_stylesheet_directory() . '/inc/jwt-handler.php';
 require_once get_stylesheet_directory() . '/inc/api/index.php';
 require_once get_stylesheet_directory() . '/inc/woo/index.php';
+
+
