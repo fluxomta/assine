0c1c62221bcd5dce598526262c3ed3a006d5feff
Tales Hs <webtailss@gmail.com>
2024-09-22 20:12:40 -0300

    remove plugins

--- wp-content/plugins/mailchimp-for-woocommerce/blocks/build/newsletter-block-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-shared-hocs', 'wc-settings', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '307219fabfd14ec3286a');
--- wp-content/plugins/mailchimp-for-woocommerce/blocks/build/newsletter-block.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-polyfill'), 'version' => '98e26ff444fbd8fc7d70');
